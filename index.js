ff8e808225301b78371c21869ea499b485e45138
ArifDwiPrasetya <arifdwiprasetya14@gmail.com>
2025-04-28 15:20:56 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const app = express()
+const PORT = 8000
+
+// app.get("/", (req, res)=>{
+//     res.send("Hello from the server!")
+// })
+
+// app.get("/cars", (req, res)=>{
+//     res.send("This is cars page uhuy")
+// })
+
+app.use(express.json())
+app.use(express.static('public'))
+
+let todos = [
+    { id: 1, task: "Belajar HTML", done: true},
+    { id: 2, task: "Belajar CSS", done: true},
+    { id: 3, task: "Belajar Javascript", done: true},
+    { id: 4, task: "Belajar Node.Js", done: true},
+    { id: 5, task: "Belajar Experss.js mantap", done: true},
+]
+
+app.get('/todos', (req, res)=>{
+    res.json(todos)
+})
+
+app.post('/todos', (req,res)=>{
+    const newTodo = req.body
+    newTodo.id = Date.now()
+    newTodo.done = false
+    todos.push(newTodo)
+    res.status(201).json({message: 'To-do berhasil ditambahkan', todo: newTodo})
+})
+
+app.delete('/todos/:id', (req, res)=>{
+    const todoId = Number(req.params.id)
+    todos = todos.filter(todo => todo.id !== todoId)
+    res.json({ message: 'To-do dihapus' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server berjalan di http://localhost:${PORT}`)
+})
